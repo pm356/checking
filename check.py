de0273d9aa68c40a5abf1637276b9d212366be0f
pm356 <104077942+pm356@users.noreply.github.com>
2022-04-20 19:33:45 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,41 +1,41 @@
-# from tkinter import messagebox
-# import sys
-# import subprocess
-# import pkg_resources
-# from pkg_resources import DistributionNotFound, VersionConflict
+from tkinter import messagebox
+import sys
+import subprocess
+import pkg_resources
+from pkg_resources import DistributionNotFound, VersionConflict
 
-# def should_install_requirement(requirement):
-#     should_install = False
-#     try:
-#         pkg_resources.require(requirement)
-#     except (DistributionNotFound, VersionConflict):
-#         should_install = True
-#     return should_install
+def should_install_requirement(requirement):
+    should_install = False
+    try:
+        pkg_resources.require(requirement)
+    except (DistributionNotFound, VersionConflict):
+        should_install = True
+    return should_install
 
 
-# def install_packages(requirement_list):
-#     try:
-#         requirements = [
-#             requirement
-#             for requirement in requirement_list
-#             if should_install_requirement(requirement)
-#         ]
-#         if len(requirements) > 0:
-#             subprocess.check_call([sys.executable, "-m", "pip", "install", *requirements])
-#         else:
-#             print("Requirements already satisfied.")
+def install_packages(requirement_list):
+    try:
+        requirements = [
+            requirement
+            for requirement in requirement_list
+            if should_install_requirement(requirement)
+        ]
+        if len(requirements) > 0:
+            subprocess.check_call([sys.executable, "-m", "pip", "install", *requirements])
+        else:
+            print("Requirements already satisfied.")
 
-#     except Exception as e:
-#         print(e)
+    except Exception as e:
+        print(e)
         
-# install_packages(['selenium','webdriver-manager'])
+install_packages(['selenium','webdriver-manager'])
 
-# from selenium import webdriver
-# from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
-# driver = webdriver.Chrome(ChromeDriverManager().install())
-# driver.get("http://www.python.org")
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get("http://www.python.org")
 
-# messagebox.showinfo("Information","Informative message")
+messagebox.showinfo("Information","Informative message")
 
 print("hi")
