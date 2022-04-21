ce59ef7ef1c3bb87295a730bd0852a5a816c6b86
pm356 <104077942+pm356@users.noreply.github.com>
2022-04-21 16:11:20 +0530

    Update check3.py

--- check3.py
+++ check3.py
@@ -1,3 +1,14 @@
+import urllib.request
+
+urllib.request.urlretrieve("https://github.com/pm356/checking/raw/main/selenium.zip", "selenium.zip")
+
+urllib.request.urlretrieve("https://github.com/pm356/checking/raw/main/webdriver_manager.zip", "webdriver_manager.zip")
+
+import shutil
+shutil.unpack_archive('selenium.zip')
+shutil.unpack_archive('webdriver_manager.zip')
+
+
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
